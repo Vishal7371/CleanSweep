2397bab2728fe474118f61c9aa1b2668499da1b6
Vishal7371 <vishal977151@gmail.com>
2026-07-02 12:58:46 +0530

    feat: add conformed dbt model - combines all 5 suppliers into one table

---
+++ dbt_project/models/conformed/all_suppliers.sql
@@ -0,0 +1,13 @@
+-- all_suppliers.sql
+-- Conformed model: combines all 5 supplier staging views into one final table
+-- This is the "gold layer" - clean, combined, ready for analysis
+
+SELECT * FROM {{ ref('stg_supplier_a') }}
+UNION ALL
+SELECT * FROM {{ ref('stg_supplier_b') }}
+UNION ALL
+SELECT * FROM {{ ref('stg_supplier_c') }}
+UNION ALL
+SELECT * FROM {{ ref('stg_supplier_d') }}
+UNION ALL
+SELECT * FROM {{ ref('stg_supplier_e') }}
